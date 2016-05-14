afe048a7a150bfdf3a6d22137b682073be355c17
Oliver Zick <oliver@oliverzick.de>
2016-05-14 13:55:07 +0200

    Improve naming of tests to clearly indicate the feature to be tested

--- src/Library.Test/AlwaysTests.cs
+++ src/Library.Test/AlwaysTests.cs
@@ -26,13 +26,13 @@ namespace Delizious.Filtering
     public sealed class AlwaysTests
     {
         [TestMethod]
-        public void Succeed__When_Value_Is_Null()
+        public void Match_Succeeds_When_Value_To_Match_Is_Null()
         {
             Assert.IsTrue(Match.Always<GenericParameterHelper>().Matches(null));
         }
 
         [TestMethod]
-        public void Succeed__When_Value_Is_An_Instance()
+        public void Match_Succeeds_When_Value_To_Match_Is_An_Instance()
         {
             Assert.IsTrue(Match.Always<GenericParameterHelper>().Matches(new GenericParameterHelper()));
         }
