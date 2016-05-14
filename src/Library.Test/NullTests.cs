b6b131a16531ef5322127358454ed1e0d07d34a3
Oliver Zick <oliver@oliverzick.de>
2016-05-14 13:58:17 +0200

    Improve naming of tests to clearly indicate the feature to be tested

--- src/Library.Test/NullTests.cs
+++ src/Library.Test/NullTests.cs
@@ -26,13 +26,13 @@ namespace Delizious.Filtering
     public sealed class NullTests
     {
         [TestMethod]
-        public void Succeed__When_Value_Is_Null()
+        public void Match_Succeeds_When_Value_To_Match_Is_Null()
         {
             Assert.IsTrue(Match.Null<GenericParameterHelper>().Matches(null));
         }
 
         [TestMethod]
-        public void Fail__When_Value_Is_An_Instance()
+        public void Match_Fails_When_Value_To_Match_Is_An_Instance()
         {
             Assert.IsFalse(Match.Null<GenericParameterHelper>().Matches(new GenericParameterHelper()));
         }
